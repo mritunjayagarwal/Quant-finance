ef9bd35b6a18fe67d3aa1817ea8c2d02b15df592
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2023-06-18 19:09:18 +0530

    New

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from './Home/Home';
 import {Routes, Route} from 'react-router-dom';
 import Swap from "./components/Swap";
 import {useAccount} from 'wagmi';
-import Trade from "./Trade/Trade";
 
 function App() {
   const {address, isConnected} = useAccount();
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/swap" element={<Swap  isConnected={isConnected} address={address} />}></Route>
-          <Route path="/trade" element = {<Trade />}></Route>
         </Routes>
       </div>
   </div>;
